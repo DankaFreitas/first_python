fb5139a0e2b01154c767062a47972677f77415b1
Oberdã <carlos-oberda@hotmail.com>
2024-01-08 11:35:13 -0300

    atualizacao

---
+++ string_3.py
@@ -0,0 +1,9 @@
+nome = "Carlos Oberdã Pedroso de Freitas"
+
+print(nome[0])
+print(nome[:6])
+print(nome[7:])
+print(nome[7:13])
+print(nome[7:13:2])
+print(nome[:])
+print(nome[::-1])
---
+++ string_4.py
@@ -0,0 +1,23 @@
+nome = "Oberdã"
+
+mensagem = f'''
+    Olá meu nome é {nome},
+Estou aprendendo Python.
+      Essa mensagem tem diferentes recuos.
+'''
+
+print(mensagem)
+
+print(''' 
+======================MENU======================
+      
+      1 - DEPOSITAR
+      2 - SACAR
+      3 - SAIR
+
+================================================
+
+      OBRIGADO POR USAR NOSSO SISTEMA!!!
+
+
+''')
